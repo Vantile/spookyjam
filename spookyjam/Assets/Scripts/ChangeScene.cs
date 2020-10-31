434fa2d82a4175999e968e496c53d5b18aecc22a
Diego Romero <36675478+diegorhs@users.noreply.github.com>
2020-11-01 00:25:15 +0100

    Añadida escena de lore

---
+++ spookyjam/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    [SerializeField] private int m_SceneIndex = 0;
+    [SerializeField] private string m_ChangeKey = "return";
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(m_ChangeKey))
+        {
+            SceneManager.LoadScene(m_SceneIndex);
+        }
+    }
+}
---
+++ spookyjam/Assets/Scripts/ChangeSceneAdditive.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneAdditive : MonoBehaviour
+{
+    [SerializeField] private int m_SceneIndex = 0;
+    [SerializeField] private string m_ChangeKey = "return";
+
+    private bool loaded = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!loaded && Input.GetKeyDown(m_ChangeKey))
+        {
+            loaded = true;
+            SceneManager.LoadScene(m_SceneIndex, LoadSceneMode.Additive);
+        }
+    }
+}
