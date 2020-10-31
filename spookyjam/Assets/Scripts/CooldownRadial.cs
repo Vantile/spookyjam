493e7d24235562201a3006c6bb04b8a1b7482cbe
javiervaloria <47637797+javiervaloria@users.noreply.github.com>
2020-10-31 16:40:10 +0100

    UI Cooldown Radial

---
+++ spookyjam/Assets/Scripts/CooldownRadial.cs
@@ -0,0 +1,57 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class CooldownRadial : MonoBehaviour
+{
+	[HideInInspector]
+	public float TotalTime = 0.0f;
+
+	private float m_RemainingTime;
+	private Image m_Image;
+
+	// Use this for initialization
+	void Start ()
+	{
+		m_Image = GetComponent<Image>();
+	}
+
+	void OnEnable()
+	{
+		// Al activarlo reseteamos el tiempo total que dura el powerup
+		m_RemainingTime = TotalTime;
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		// TODO 1 - Comprobamos si se ha acabado el tiempo
+		if (m_RemainingTime <= 0)
+		{
+			// TODO 2 - Desactivamos el gameobject para que no se pinte
+			//gameObject.SetActive(false);
+		}
+		else
+		{
+			// TODO 3 - Calculamos cuánto powerup hay que pintar (entre 0 y 1)
+			// dependiendo del tiempo que nos queda
+			float portion = Mathf.InverseLerp(TotalTime, 0, m_RemainingTime);
+            if (portion > 0.98)
+            {
+				portion = 1;
+            }
+			// TODO 4 - Asignamos este valor al fillAmount de la imagen
+			m_Image.fillAmount = portion;
+			// TODO 5 - Restamos al tiempo restante el tiempo que ha pasado
+			m_RemainingTime -= Time.deltaTime;
+		}
+
+		
+	}
+	public void StartCooldown(float duration)
+	{
+		TotalTime = duration;
+		m_RemainingTime = duration;
+	}
+}
---
+++ spookyjam/Assets/Scripts/GameManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+    public GameObject CooldownFire;
+    public GameObject CooldownFreeze;
+    public GameObject CooldownRock;
+
+    //Singleton
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("z")) 
+        {
+            Debug.Log("pulsa z");
+            CooldownRadial timer = CooldownFire.GetComponent<CooldownRadial>();
+            if(timer != null)
+            {
+                timer.StartCooldown(5);
+            }
+        }
+
+        if (Input.GetKeyDown("x"))
+        {
+            Debug.Log("pulsa x");
+            CooldownRadial timer = CooldownFreeze.GetComponent<CooldownRadial>();
+            if (timer != null)
+            {
+                timer.StartCooldown(5);
+            }
+        }
+
+        if (Input.GetKeyDown("c"))
+        {
+            Debug.Log("pulsa c");
+            CooldownRadial timer = CooldownRock.GetComponent<CooldownRadial>();
+            if (timer != null)
+            {
+                timer.StartCooldown(5);
+            }
+        }
+    }
+
+
+    //Function called when the player loses
+    public void GameOver()
+    {
+        //TODO: Implement this
+        print("The player has lost the game.");
+    }
+
+
+}
