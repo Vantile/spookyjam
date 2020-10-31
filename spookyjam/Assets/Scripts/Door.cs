29664e5924ad8486c10fe90495f9990629c58a25
cparcas <carlos.parrado@rai.usc.es>
2020-10-31 12:48:39 +0100

    Scripts puerta
    Carpeta scritps
    Script de puerta
    Script de accionador de puerta

---
+++ spookyjam/Assets/Scripts/Door.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+
+    public enum State { CLOSED, OPEN } //dos estados abierta y cerrada
+    public int id_door; //numero identificador de cada puerta
+    public string door_open;
+    public Animation door_animation;
+    public AudioSource door_audio;
+
+    private State door_state = State.CLOSED; //inicialmente esta cerrada
+
+
+    public State DoorState
+    {
+        get { return door_state; }
+    }
+
+    void OnTriggerActivate(int activateDoor)
+    {
+        if (DoorState == State.CLOSED)
+        {
+            if (activateDoor == id_door)//si el numero que recibe al activar es el de esta puerta, se abre
+            {
+                door_state = State.OPEN;
+                door_animation.Play(door_open);
+                door_audio.Play();
+            }
+        }
+    }
+
+}
---
+++ spookyjam/Assets/Scripts/Lever.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour
+{
+    public enum State { ACTIVATED, DISABLED } //dos estados activado y desactivado
+    public int id_lever; //id que coincide con la puerta que abre
+    public string lever_activate;
+    public Animation lever_animation;
+
+    private State lever_state = State.DISABLED;
+
+    public State LeverState
+    {
+        get { return lever_state; }
+    }
+    void OnTriggerActivate(int activateLever)
+    {
+        if (LeverState == State.DISABLED)
+        {
+            if (activateLever == id_lever)
+            {
+                lever_state = State.ACTIVATED;
+                lever_animation.Play(lever_activate);
+                //lanzar interfaz de que se ha abierto una puerta
+            }
+        }
+    }
+}
