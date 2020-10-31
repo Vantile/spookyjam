928b050c1bef6aedaf0c5cd0ca7c7cfb74309c6f
Diego Romero <36675478+diegorhs@users.noreply.github.com>
2020-11-01 00:57:57 +0100

    Añadido easter egg

---
+++ spookyjam/Assets/Scripts/EasterEgg.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EasterEgg : MonoBehaviour
+{
+    [SerializeField] private AudioSource m_Sound;
+
+
+    private bool found = false;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!found && other.tag == "Player")
+        {
+            found = true;
+            m_Sound.Play();
+        }
+    }
+}
