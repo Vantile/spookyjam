9f38855d5b83e4295c2bace06ec3239c698a418b
Diego Romero <36675478+diegorhs@users.noreply.github.com>
2020-10-31 12:53:01 +0100

    Añadido personaje
    - prefab del personaje
    - script para controlar al personaje
    - script de la camara

---
+++ spookyjam/Assets/Scripts/CharacterUserControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterUserControl : MonoBehaviour
+{
+    [SerializeField] private float speed = 1;
+    [SerializeField] private string _powerButton = "space";
+
+    private Rigidbody m_Rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 movement = h * Vector3.right + v * Vector3.forward;
+        m_Rigidbody.AddForce(movement * speed);
+
+        //bool power = Input.GetKey(_powerButton);
+    }
+}
