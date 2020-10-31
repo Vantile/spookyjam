65d45320ca3136fb5845c5395795c2d182828df3
Diego Romero <36675478+diegorhs@users.noreply.github.com>
2020-10-31 22:59:50 +0100

    Añadidos iconos a los activadores de powers

---
+++ spookyjam/Assets/Scripts/Rotation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    public Vector3 m_RotationSpeed = Vector3.zero;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(m_RotationSpeed * Time.deltaTime);
+    }
+}
