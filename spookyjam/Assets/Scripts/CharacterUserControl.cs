25ab8de7195a22cd9dda4f64cf108e01aa61c6f7
Diego Romero <36675478+diegorhs@users.noreply.github.com>
2020-10-31 19:52:58 +0100

    Añadido phasing de zonas

--- spookyjam/Assets/Scripts/CharacterUserControl.cs
+++ spookyjam/Assets/Scripts/CharacterUserControl.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class CharacterUserControl : MonoBehaviour
 {
-    [SerializeField] private float speed = 50000;
+    [SerializeField] private float speed = 1;
     [SerializeField] private string _powerButton = "space";
 
     private Rigidbody2D m_Rigidbody;
-    Animator m_anim;
+    Animator m_Animator;
+    private string powerName;
+    private bool powerEnabled = false;
+    private GameObject phasingZone = null;
+    private Dictionary<string, bool> powerReady;
 
     // Start is called before the first frame update
     void Start()
     {
         m_Rigidbody = GetComponent<Rigidbody2D>();
-        m_anim = GetComponent<Animator>();
+        m_Animator = GetComponent<Animator>();
+        powerReady["Fire"] = false;
+        powerReady["Ice"] = false;
+        powerReady["Rock"] = false;
     }
 
     private void FixedUpdate()
@@ -24,9 +31,33 @@ public class CharacterUserControl : MonoBehaviour
 
         Vector2 movement = h * Vector2.right + v * Vector2.up;
         m_Rigidbody.velocity = (movement * speed);
-        m_anim.SetFloat("XSpeed", m_Rigidbody.velocity.x);
-        m_anim.SetFloat("YSpeed", m_Rigidbody.velocity.y);
+        m_Animator.SetFloat("XSpeed", m_Rigidbody.velocity.x);
+        m_Animator.SetFloat("YSpeed", m_Rigidbody.velocity.y);
 
-        //bool power = Input.GetKey(_powerButton);
+        if (Input.GetKey(_powerButton) && powerEnabled && powerReady[powerName] && phasingZone != null)
+            phasingZone.SetActive(false);
+    }
+
+    public void setPhasing(string power, bool enabled, GameObject zone)
+    {
+        powerName = power;
+        powerEnabled = enabled;
+
+        if (!enabled)
+        {
+            // TODO Activar cooldown del poder
+        }
+
+        if (zone != null)
+        {
+            Transform parentZone = zone.transform.parent;
+            for (int i = 0; i < parentZone.childCount; i++)
+            {
+                if (parentZone.GetChild(i).tag == powerName + "Zone")
+                {
+                    GameObject phasingZone = parentZone.GetChild(i).gameObject;
+                }
+            }
+        }
     }
 }
