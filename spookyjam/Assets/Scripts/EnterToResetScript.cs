de0b7e9ef847d76dcdd9865f8f3939ae8b090298
Vantile <vantilesyfon@gmail.com>
2020-10-31 15:05:19 +0100

    Script para resetear juego al pulsar ENTER en game over

---
+++ spookyjam/Assets/Scripts/EnterToResetScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnterToResetScript : MonoBehaviour
+{
+    [SerializeField]
+    private int mainGameSceneIndex = 0;
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Return)) {
+            SceneManager.LoadScene(mainGameSceneIndex);
+        }
+    }
+}
